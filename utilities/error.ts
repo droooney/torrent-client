fee72aecd13e7ffdc2b86696308d82701a8f0ade
droooney <d.vor7@mail.ru>
2024-01-12 16:25:24 +0500

    better error logging

--- utilities/error.ts
+++ utilities/error.ts
@@ -3,6 +3,19 @@ import CustomError from 'utilities/CustomError';
 export function prepareErrorForLogging(err: unknown): string {
   return err instanceof Error
     ? `${err.stack ?? err.message}${
+        err instanceof AggregateError
+          ? `
+    [Aggregated from]: ${
+      err.errors
+        .map(
+          (err) => `
+        ${prepareErrorForLogging(err).replace(/\n/g, '\n        ')}`,
+        )
+        .join('') || '<empty>'
+    }
+    `
+          : ''
+      }${
         err.cause
           ? `
     [Caused by]: ${prepareErrorForLogging(err.cause).replace(/\n/g, '\n    ')}`
