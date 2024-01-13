3412fbae67bcdfc6a468d93238349d6aa32d8fb8
droooney <d.vor7@mail.ru>
2024-01-13 05:29:51 +0500

    improve error logging

--- main.ts
+++ main.ts
@@ -2,3 +2,11 @@ import 'utilities/importEnv';
 
 import 'telegram-bot';
 // import 'web-server';
+
+import { prepareErrorForLogging } from 'utilities/error';
+
+process.on('uncaughtException', (err) => {
+  console.log(prepareErrorForLogging(err));
+
+  process.exit(1);
+});
