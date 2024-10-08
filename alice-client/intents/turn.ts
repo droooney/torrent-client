bf34a7cf541a03bdb00cb1e49faf5ca456bb699e
droooney <d.vor7@mail.ru>
2024-10-08 23:11:30 +0500

    alice: turn off

--- alice-client/intents/turn.ts
+++ alice-client/intents/turn.ts
@@ -28,3 +28,25 @@ aliceClient.handleIntent(IntentType.TURN_ON, async ({ slots }) => {
     text: `Включаю ${device.name}`,
   });
 });
+
+aliceClient.handleIntent(IntentType.TURN_OFF, async ({ slots }) => {
+  const { target } = slots;
+
+  if (target?.type !== 'YANDEX.STRING') {
+    throw new CustomError(ErrorCode.UNSUPPORTED, 'Неизвестное устройство');
+  }
+
+  const device = await devicesClient.findDevice(target.value);
+
+  if (!device) {
+    throw new CustomError(ErrorCode.NOT_FOUND, 'Устройство не найдено');
+  }
+
+  runTask(async () => {
+    await devicesClient.turnOffDevice(device.id);
+  });
+
+  return new VoiceResponse({
+    text: `Выключаю ${device.name}`,
+  });
+});
